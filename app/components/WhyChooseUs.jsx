e2ac480220b717f247bb5a335fe08314546f8aa8
prayagpc <prayagpc28@gmail.com>
2024-12-03 01:50:48 +0530

    Added whyChooseSolveKard Component

--- app/components/WhyChooseUs.jsx
+++ app/components/WhyChooseUs.jsx
@@ -1,34 +1,4 @@
-// import { FaHandshake, FaClipboardList, FaChartLine, FaLeaf, FaPaintBrush } from 'react-icons/fa';
-
 const WhyChooseUs = ({data}) => {
-  // const features = [
-  //   {
-  //     title: "Simplify Networking",
-  //     description: "Traditional business cards are outdated and inconvenient. SolveKard's NFC-enabled cards offer instant sharing with just a tap or scan—no apps required.",
-  //     icon: <FaHandshake className="w-8 h-8" />
-  //   },
-  //   {
-  //     title: "Stay Organized",
-  //     description: "Ditch the pile of lost contacts. SolveKard's platform helps you organize, tag, and prioritize leads, ensuring no opportunity slips through the cracks.",
-  //     icon: <FaClipboardList className="w-8 h-8" />
-  //   },
-  //   {
-  //     title: "Track Your Impact",
-  //     description: "With analytics and CRM integrations, you can monitor engagement and turn casual introductions into meaningful relationships.",
-  //     icon: <FaChartLine className="w-8 h-8" />
-  //   },
-  //   {
-  //     title: "Eco-Friendly Connections",
-  //     description: "Save paper, reduce waste, and embrace a sustainable way to network—because smarter connections shouldn't cost the planet.",
-  //     icon: <FaLeaf className="w-8 h-8" />
-  //   },
-  //   {
-  //     title: "Customizable & Scalable",
-  //     description: "Whether you're a freelancer or an enterprise team, SolveKard offers personalized designs and solutions to match your style and scale your business.",
-  //     icon: <FaPaintBrush className="w-8 h-8" />
-  //   }
-  // ];
-
     return (
       <section className="py-20 px-4 relative z-20">
         <div className="max-w-7xl mx-auto">
